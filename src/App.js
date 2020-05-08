092d585f40d98d6d1c0f785639a0d340efdf82a6
vt1022 <57807628+vt1022@users.noreply.github.com>
2020-05-08 15:08:31 -0400

    adapting dndGrid.js to take array from App.js state

--- src/App.js
+++ src/App.js
@@ -19,7 +19,38 @@ class App extends Component {
         currentPage : 'landing',
         generatedWords: [] ,
         // push each word into poem array when dragged into staging area
-        poem : []
+        poem : [],
+        generatedWordsTest: [
+            {id: '123', order: 0, content: 'a'},
+            {id: '124', order: 1, content: 'an'},
+            {id: '125', order: 2, content: 'the'},
+            {id: '126', order: 3, content: 'that'},
+            {id: '127', order: 4, content: 'this'},
+            {id: '128', order: 5, content: 'those'},
+            {id: '129', order: 6, content: 'these'},
+            {id: '120', order: 7, content: 'my'},
+            {id: '131', order: 8, content: 'your'},
+            {id: '132', order: 9, content: 'their'},
+            {id: '133', order: 10, content: 'our'},
+            {id: '134', order: 11, content: 'ours'},
+            {id: '135', order: 12, content: 'whose'},
+            {id: '136', order: 13, content: 'his'},
+            {id: '137', order: 14, content: 'hers'},
+            {id: '138', order: 15, content: 'its'},
+            {id: '139', order: 16, content: 'which'},
+            {id: '130', order: 17, content: 'some'},
+            {id: '141', order: 18, content: 'both'},
+            {id: '142', order: 19, content: 'most'},
+            {id: '143', order: 20, content: 'many'},
+            {id: '144', order: 21, content: 'a few'},
+            {id: '145', order: 22, content: 'a lot of'},
+            {id: '146', order: 23, content: 'any'},
+            {id: '147', order: 24, content: 'much'},
+            {id: '148', order: 25, content: 'a little'},
+            {id: '149', order: 26, content: 'enough'},
+            {id: '140', order: 27, content: 'several'},
+            {id: '151', order: 28, content: 'none'},
+        ]
         }
     }
 
@@ -95,7 +126,7 @@ class App extends Component {
                             <GameBoard />
                         || 
                         currentPage === 'dndGrid' &&
-                            <DndGrid />
+                            <DndGrid generatedWordsTest={this.state.generatedWordsTest}/>
                         || 
                         currentPage === 'gallery' &&
                             <Gallery changePage={this.changePage}/>
