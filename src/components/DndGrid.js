9ab959ead0095d487acefaa0b97bce7cfb0306bd
Zarina M <zarinabliss@gmail.com>
2020-05-10 17:04:52 -0400

    added error handling to poem wordcount

--- src/components/DndGrid.js
+++ src/components/DndGrid.js
@@ -42,16 +42,22 @@ class DndGrid extends Component {
         list[sourceIndex].order = (list[destinationIndex].order + list[destinationIndex + 1].order) / 2;
         this.sortList();
     }
+
     
     generatedWordClick = (e, wordObject) => {
-        const { id, content } = wordObject
-        const newList = [...this.state.sortedList]
-        // if statement to filter out words already used
-        newList.push({id: id, order: this.state.wordOrder, content: content});
-        this.setState({
-            sortedList: sortList(newList),
-            wordOrder: this.state.wordOrder + 1
-        })
+        console.log(this.state.wordOrder)
+        if (this.state.wordOrder >= 20) {
+            alert("You've reached your maximum word limit")
+        } else {
+            const { id, content } = wordObject
+            const newList = [...this.state.sortedList]
+            // if statement to filter out words already used
+            newList.push({id: id, order: this.state.wordOrder, content: content});
+            this.setState({
+                sortedList: sortList(newList),
+                wordOrder: this.state.wordOrder + 1
+            })
+        }
     }
 
     render() {
